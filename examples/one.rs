706031d800c9562dffb4bb3ed6c518326edbec42
Jan Algermissen <algermissen@acm.org>
2021-08-04 22:46:02 +0200

    -

--- examples/one.rs
+++ examples/one.rs
@@ -1,43 +1,87 @@
 #![deny(warnings)]
+#![feature(unboxed_closures)]
+#![feature(fn_traits)]
 use hexarch::AppError;
 use async_trait::async_trait;
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
-    let repo = Repo {conn: "xxx".into()};
-    let uc = GetAFoo::new(repo);
-    let y = uc.apply(42).await?;
-    println!("{}",y);
+    // Create the repo
+    let user_repo = UserRepo { _connection: "proto:mydb/foo".into()};
+
+    // Create the use case
+    let get_user = GetUser::new(user_repo);
+
+    // Supply use case to handler.
+    api_handler_get_user(get_user).await?;
+
     Ok(())
 }
 
-// The 'interface' that the repo has to implement. it is defined in the use case layer
-// like we do in go
-#[async_trait]
-pub trait FooLoader {
-    async fn load_foo(self, id: i32) -> Result<String, AppError>;
+// ------------------------------------------------------------------------------------------
+// API
+async fn api_handler_get_user<T: UserLoader + 'static>(use_case: GetUser<T>) -> Result<(), AppError> {
+    let user_id = 42;  // Eg read input from somewhere
+    let user = use_case.apply(user_id).await?;
+    //let user = use_case(user_id).await?;
+    println!("User: {}",user.name);
+    Ok(())
 }
 
-pub struct Repo {
-    conn: String
+
+// ------------------------------------------------------------------------------------------
+// Entities
+pub struct User {
+    name:String
+}
+
+// ------------------------------------------------------------------------------------------
+// Use Cases
+#[async_trait]
+pub trait UserLoader {
+    async fn load_user(self, id: i32) -> Result<User, AppError>;
+}
+
+pub struct GetUser<T> {
+    user_loader: T,
+}
+impl<T> GetUser<T> {
+    pub fn new(user_loader: T) -> GetUser<T> {
+        GetUser { user_loader }
+    }
+}
+
+impl<T: UserLoader + 'static> GetUser<T> {
+    pub fn apply(self, id: i32) -> impl std::future::Future<Output=Result<User, AppError>> + 'static {
+        //Box::new(self.user_loader.load_user(id))
+        self.user_loader.load_user(id)
+    }
+}
+
+// ------------------------------------------------------------------------------------------
+// Adapters
+pub struct UserRepo {
+    _connection: String
 }
 
 #[async_trait]
-impl FooLoader for Repo {
-    async fn load_foo(self, _id: i32) -> Result<String, AppError> {
-        let foo: String = self.conn.clone();
-        Ok(foo)
+impl UserLoader for UserRepo {
+    async fn load_user(self, id: i32) -> Result<User, AppError> {
+        let user = User{name: format!("User {}",id)};
+        Ok(user)
     }
 }
 
 
+
+// ------------------------------------------------------------------------------------------
 // This struct will hold the repository - by using a struct here, instead of
 // using a makeUseCase() function, we have the benefit that the result is
 // a struct type, not some strange compile time inferred Fn type - this makes
 // it much easier to deal with lifetimes (I think) - in the end, Rust closures are
 // structs anyway, so there really is not much of a difference
 // pub struct GetAFoo<'a,T> {
-//     loader: T,
+//     user_loader: T,
 //     phantom: std::marker::PhantomData<&'a T>,
 // }
 // // just a constructor for the use case ( similar to the MakeUseCase..  functions we use in Go)
@@ -48,7 +92,7 @@ impl FooLoader for Repo {
 //
 // }
 // Make the use case callable with i32 IDs.
-// impl<'a,T: FooLoader + 'static> FnOnce<(i32,)> for GetAFoo<'a,T> {
+// impl<'a,T: UserLoader + 'static> FnOnce<(i32,)> for GetAFoo<'a,T> {
 //     type Output = Box<dyn std::future::Future<Output=Result<entities::Foo, error::AppError>>>;
 //     extern "rust-call" fn call_once(self, args: (i32,)) -> Box<dyn std::future::Future<Output=Result<entities::Foo, error::AppError>>> {
 //         Box::new(self.loader.load_foo(args.0))
@@ -79,41 +123,21 @@ impl FooLoader for Repo {
 // Meaning: we can leverage the fact that we can turn any struct into a closure by implementing
 // FnOnce, FnMut, or Fn traits on it.
 
-pub struct GetAFoo<T> {
-    loader: T,
-}
-// just a constructor for the use case ( similar to the MakeUseCase..  functions we use in Go)
-impl<T> GetAFoo<T> {
-    pub fn new(loader: T) -> GetAFoo<T> {
-        GetAFoo { loader }
-    }
-}
 
-// impl<T: FooLoader + 'static> GetAFoo<T> {
-//     pub fn apply(&self, id: i32) -> Box<dyn std::future::Future<Output=Result<String, AppError>> + '_> {
-//          Box::new(self.loader.load_foo(id))
-//      }
-// }
-impl<T: FooLoader + 'static> GetAFoo<T> {
-    pub fn apply(self, id: i32) -> impl std::future::Future<Output=Result<String, AppError>> + 'static {
-        //Box::new(self.loader.load_foo(id))
-        self.loader.load_foo(id)
-    }
-}
 
-// impl<T: FooLoader + 'static> FnOnce<(i32,)> for GetAFoo<T> {
-//     type Output = Box<dyn std::future::Future<Output=Result<String, error::AppError>>>;
-//     extern "rust-call" fn call_once(self, args: (i32,)) -> Box<dyn std::future::Future<Output=Result<String, AppError>>> {
-//
+// impl<T: UserLoader + 'static> FnOnce<(i32,)> for GetAFoo<T>
+// {
+//     type Output = Box<dyn std::future::Future<Output=Result<User, AppError>>>;
+//     extern "rust-call" fn call_once(self, args: (i32,)) -> Box<dyn std::future::Future<Output=Result<User, AppError>>> {
 //         Box::new(self.loader.load_foo(args.0))
 //     }
 // }
-// impl<T: 'static + FooLoader> FnMut<(i32,)> for GetAFoo<T> {
+// impl<T: 'static + UserLoader> FnMut<(i32,)> for GetAFoo<T> {
 //     extern "rust-call" fn call_mut(&mut self, args: (i32,)) -> Box<dyn std::future::Future<Output=Result<entities::Foo, error::AppError>>> {
 //         Box::new(self.loader.load_foo(args.0))
 //     }
 // }
-// impl<T: 'static + FooLoader> Fn<(i32,)> for GetAFoo<T> {
+// impl<T: 'static + UserLoader> Fn<(i32,)> for GetAFoo<T> {
 //     extern "rust-call" fn call(&self, args: (i32,)) -> Box<dyn std::future::Future<Output=Result<entities::Foo, error::AppError>>> {
 //         Box::new(self.loader.load_foo(args.0))
 //     }
